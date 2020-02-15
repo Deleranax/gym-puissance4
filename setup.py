8d89ef608a1c3098e21fa8d16b88cba331cb701a
Deleranax <9z3KI8ininGZmy>
2020-02-15 20:23:14 +0100

    Ajout du GUI

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='gym_puissance4',
     packages=setuptools.find_packages(),
-    version='0.18',
+    version='1.0',
     license='MIT',
     description='Environement Gym pour le jeu Puissance4',
     author='Deleranax',
